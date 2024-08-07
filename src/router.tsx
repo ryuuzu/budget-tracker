c930e88325387dc20d84ababb186b9db9a0baa88
Utsav Gurmachhan <me@ryuuzu.xyz>
2024-08-07 12:57:25 +0545

    updated component names in route

--- src/router.tsx
+++ src/router.tsx
@@ -1,16 +1,23 @@
 import { createBrowserRouter } from 'react-router-dom';
 
 import AuthLayout from './layouts/AuthLayout';
-import LoginPage from './pages/authentication/LoginPage';
-import RegisterPage from './pages/authentication/RegisterPage';
+import AuthenticatedLayout from './layouts/AuthenticatedLayout';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/authentication/Login';
+import Register from './pages/authentication/Register';
 
 export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AuthenticatedLayout />,
+    children: [{ path: '/', element: <Dashboard /> }],
+  },
   {
     path: 'auth',
     element: <AuthLayout />,
     children: [
-      { path: 'login', element: <LoginPage /> },
-      { path: 'register', element: <RegisterPage /> },
+      { path: 'login', element: <Login /> },
+      { path: 'register', element: <Register /> },
     ],
   },
 ]);
