ad4d6f1202216224d33d2a990857d7674c924b4f
Utsav Gurmachhan <me@ryuuzu.xyz>
2024-08-07 12:58:25 +0545

    fixed layout

---
+++ src/layouts/AuthenticatedLayout.tsx
@@ -0,0 +1,26 @@
+import { useEffect } from 'react';
+import { Outlet } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
+import { useAuth } from '@/components/contexts/auth-provider';
+
+const AuthenticatedLayout = () => {
+  const { user } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user) {
+      navigate('/auth/login');
+    }
+  }, [user, navigate]);
+
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <div className="w-[350px] md:w-[400px]">
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default AuthenticatedLayout;
